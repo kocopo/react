9cfc9166d1acf93674079b9210eb65015d323bfa
Otakar Kočka <o.kocka@okocka.cz>
2022-08-30 15:35:39 +0200

    cont

--- react-basics/src/components/ExpenseItem.jsx
+++ react-basics/src/components/ExpenseItem.jsx
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+const expenseDate = new Date(2021, 2, 27);
+const expenseTitle = 'Card Insurance';
+const expenseAmount = 7500;
   return (
     <div className="expense-item">
-      <div>27.3.2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">7500Kč</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}Kč</div>
       </div>
     </div>
   );
