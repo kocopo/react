0decaa97246ddac1c75f7cddb7145a626039c614
otakar.kocka <otakar.kocka@bootiq.io>
2022-09-09 19:41:08 +0200

    Wrapper

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -25,11 +25,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem amount={expenses[0].amount} title={expenses[0].title} date={expenses[0].date} />
-      <ExpenseItem amount={expenses[1].amount} title={expenses[1].title} date={expenses[1].date} />
-      <ExpenseItem amount={expenses[2].amount} title={expenses[2].title} date={expenses[2].date} />
-      <ExpenseItem amount={expenses[3].amount} title={expenses[3].title} date={expenses[3].date} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
---
+++ react-basics/src/components/Expenses.jsx
@@ -0,0 +1,21 @@
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem'
+import Card from './Card'
+function Expenses(props){
+    const expenses = props.expenses;
+    const expenseRows = [];
+    for(let i = 0; i < expenses.length; i++){
+        expenseRows.push(<ExpenseItem 
+            amount={expenses[i].amount} 
+            title={expenses[i].title} 
+            date={expenses[i].date} />);
+    }
+
+    return(
+<Card className='expenses'>
+    {expenseRows}
+</Card>
+    );
+}
+
+export default Expenses;
